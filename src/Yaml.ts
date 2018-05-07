cc488231c6fb8075de9a61dad1dbbad09d2da502
Stephen Haberman <stephen.haberman@gmail.com>
2018-05-07 15:22:24 -0500

    Basic two-level Yaml output.

--- src/Yaml.ts
+++ src/Yaml.ts
@@ -1,16 +1,46 @@
 
+/**
+ * Yaml-generated helper class.
+ *
+ * Currently very low-level/stack-ish, e.g.:
+ *
+ * - push(key) creates a new level,
+ * - put(key, value) puts a simple/list value at current level
+ * - pop() goes up a level
+ */
 export class Yaml {
   private current: any = {};
+  private stack: any[] = [];
 
   put(key: string, value: any) {
     this.current[key] = value;
   }
 
+  push(key: string) {
+    const newLevel = {};
+    this.current[key] = newLevel;
+    this.stack.push(this.current);
+    this.current = newLevel;
+  }
+
+  pop() {
+    this.current = this.stack.pop();
+  }
+
   output(): string {
     let s = '';
-    Object.keys(this.current).forEach(k => {
-      s += `${k}: ${this.current[k]}`;
-    });
+    function concat(prefix: string, o: any) {
+      Object.keys(o).forEach(k => {
+        const v = o[k];
+        if (v instanceof Object) {
+          s += `${prefix}${k}:\n`
+          concat(prefix + ' ', v);
+        } else {
+          s += `${prefix}${k}: ${o[k]}\n`;
+        }
+      });
+    }
+    concat('', this.current);
     return s;
   }
 }
--- tests/Yaml.test.ts
+++ tests/Yaml.test.ts
@@ -6,3 +6,19 @@ it('should generate basic yaml', () => {
   y.put('key', 'value');
   expect(y.output()).toMatchSnapshot();
 });
+
+it('should generate two lines', () => {
+  const y = new Yaml();
+  y.put('key1', 'value1');
+  y.put('key2', 'value2');
+  expect(y.output()).toMatchSnapshot();
+});
+
+it('should generate a child', () => {
+  const y = new Yaml();
+  y.put('key1', 'value1');
+  y.push('key2');
+  y.put('child1', 'value1');
+  y.pop();
+  expect(y.output()).toMatchSnapshot();
+});
